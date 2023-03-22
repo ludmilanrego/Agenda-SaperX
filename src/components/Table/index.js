b13201b860825468a12b4a6eab31920a4a064a43
Ludmila <ludmilanrego@gmail.com>
2023-03-22 10:08:02 -0300

    add functions

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,11 +1,71 @@
 import './styles.css';
 import TableRow from '../TableRow';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import UserContext from '../../contexts/UserContext';
+
+import NavRight from '../../assets/navigate_right.png';
+import NavLeft from '../../assets/navigate_left.png';
 
 export default function Table() {
 
     const { contactsList } = useContext(UserContext)
+    const [page, setPage] = useState(1)
+    const maxNumberPerPage = 5
+
+
+    function ajustPage(list) {
+
+        const itemsCountEnd = (maxNumberPerPage * page);
+        const itemsCountStart = maxNumberPerPage * (page - 1);
+
+        return list.slice(itemsCountStart, itemsCountEnd)
+    }
+
+    function decreasePage(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (page === 1) {
+            return
+        }
+
+        const decreasedPage = page - 1
+        setPage(decreasedPage)
+    }
+
+    function increasePage(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (((page * maxNumberPerPage)) > contactsList.length) {
+            return
+        }
+
+        const increasedPage = page + 1
+        setPage(increasedPage)
+    }
+
+
+    function sortList(list) {
+
+        const sortedList = list.sort(function (a, b) {
+            if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                return 1;
+            }
+            if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                return -1;
+            }
+            return 0;
+        });
+        return sortedList
+    }
+
+    function editList(list) {
+        const sortedList = sortList(list)
+        const ajustedPagelist = ajustPage(sortedList)
+
+        return ajustedPagelist
+    }
 
     return (
         <div className='table'>
@@ -33,13 +93,26 @@ export default function Table() {
                 <div className='table-row-section icons'></div>
             </div>
             {
-                contactsList.map((contact) => (
+                editList(contactsList).map((contact) => (
                     <TableRow
                         key={contact.id}
                         contact={contact}
                     ></TableRow>
                 ))
             }
+            <div className='table-footer'>
+                <img
+                    className="exit-img"
+                    src={NavLeft}
+                    onClick={(event) => decreasePage(event)}
+                ></img>
+                <img
+                    className="exit-img"
+                    src={NavRight}
+                    onClick={(event) => increasePage(event)}
+                ></img>
+            </div>
+
         </div>
     )
 }
